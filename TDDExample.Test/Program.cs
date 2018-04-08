35ebe5eb3392721d2297a793dcda4af9146eafa4
allam <allam@DESKTOP-I3O4C6T>
2018-04-08 18:14:59 +0200

    enleve

---
+++ TDDExample.Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TDDExample.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int racineCube = Convert.ToInt32(Math.Pow(x, 1.0 / 3.0));
+
+            for (int i = 0; i < racineCube; i++)
+            {
+                if (Math.Pow(i, 3) == x)
+                {
+                    
+                }
+
+            }
+
+           
+        }
+    }
+}
